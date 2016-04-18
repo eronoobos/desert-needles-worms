f1c625b6ee4fae567cf30b07459ce818d693bb7f
eronoobos <eronoobos@gmail.com>
2016-04-17 17:00:39 -0700

    add worm-specific map options

--- mapoptions.lua
+++ mapoptions.lua
@@ -103,8 +103,90 @@ local options = {
 			{ key = 'insane', name = "Insane", desc = "500 metal per rock + more rocks, 500 energy per plant." },
 			{ key = 'none', name = "None", desc = "Zero metal/energy" },
 		},
-	},	
+	},
 
+	{
+    key    = '1_terrain_sect',
+    name   = 'Terrain Settings',
+    desc   = 'Settings affecting sand and rock.',
+    type   = 'section',
+	},
+
+	{
+		key	= 'restrict_sand_building',
+		name	= 'Restrict Building on Sand',
+		desc	= 'Non-metal-extracting buildings sink into the sand.',
+		type	= 'bool',
+		section = '1_terrain_sect',
+		def	= 'true',
+	},
+	
+	{
+		key	= 'sink_wrecks',
+		name	= 'Wrecks Sink Into Sand',
+		desc	= 'Wrecks slowly sink into the sand (not really--they lose health).',
+		type	= 'bool',
+		section = '1_terrain_sect',
+		def	= 'false',
+	},
+
+	{
+    key    = '2_sworm_sect',
+    name   = 'Sand Worm Settings',
+    desc   = 'Settings for sand worms.',
+    type   = 'section',
+	},
+	
+	{
+		key	= 'sand_worms',
+		name	= 'Sand Worms',
+		desc	= 'Sand worms will eat your units on the sand.',
+		type	= 'bool',
+		section = '2_sworm_sect',
+		def	= 'true',
+	},
+
+	{
+		key	= 'sworm_aggression',
+		name	= 'Aggression',
+		desc	= 'How much do your units on the sand attract more worms?',
+		type   = "number",
+		section = '2_sworm_sect',
+		   def    = 5,
+		   min    = 1,
+		   max    = 10,
+		   step   = 1,
+	},
+	
+	{
+		key	= 'sworm_worm_speed',
+		name	= 'Speed',
+		desc	= 'How fast the worm moves under the sand.',
+		type   = "number",
+		section = '2_sworm_sect',
+		   def    = 1,
+		   min    = 0.5,
+		   max    = 2,
+		   step   = 0.25,
+	},
+
+	{
+		key	= 'sworm_eat_mex',
+		name	= 'Eats Metal Extractors',
+		desc	= 'If checked, sand worms will eat metal extractors.',
+		type   = "bool",
+		section = '2_sworm_sect',
+		def    = 'false',
+	},
+
+	{
+		key	= 'sworm_eat_commander',
+		name	= 'Eats Commanders',
+		desc	= 'If checked, sand worms will eat commanders.',
+		type   = "bool",
+		section = '2_sworm_sect',
+		def    = 'false',
+	},
 	
 }
 
